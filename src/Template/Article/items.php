acd0711ffb46cdf6a6fa591c8bcae0087e3396b4
gogaloo27 <goga.iosava@omt.de>
2022-03-15 17:08:49 +0400

    Update items.php

--- src/Template/Article/items.php
+++ src/Template/Article/items.php
@@ -42,7 +42,7 @@ $itemsDisplayed = 1;
             > 
                 <span id="load_more_button_text">Weitere Artikel</span> <img id="spinner_l" class="loader_spin_" src="https://www.omt.de/uploads/2022/03/loader_.svg">
             </button>
-        <div id="loading-content" class=" loader_spin_">
+        <div id="after_load" class="hide_loading">
             <span>Artikel werden geladen</span>
             <img id="spinner_l"  src="/uploads/2022/03/loader_.svg">
         </div>
