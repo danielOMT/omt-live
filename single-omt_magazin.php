46c316371d0e2ea2f200e9e917786e4c0a864254
danielOMT <daniel.voelskow@reachx.de>
2022-03-16 11:47:16 +0100

    fix error on magazin ausgabe1 without print product

--- single-omt_magazin.php
+++ single-omt_magazin.php
@@ -28,34 +28,60 @@ if ($magazineProduct || $productVariation) {
 <?php get_header(); ?>
 
 <?php if (getPost()->field('downloads_use_new_template', 'bool')) : ?>
-    <?php echo DownloadView::loadTemplate('single', [
-        'title' => get_the_title(),
-        'leftImageUrl' => getPost()->field('left_image', 'image_array'),
-        'content' => getPost()->field('inhaltseditor', 'content'),
-        'peopleHeadline' => getPost()->field('downloads_people_headline', 'text'),
-        'people' => getPost()->field('downloads_people', 'repeater'),
-        'reviews' => getPost()->field('downloads_reviews', 'repeater'),
-        'firstContent' => getPost()->field('downloads_content_1', 'content'),
-        'firstContentImage' => getPost()->field('downloads_content_image_1', 'image_array'),
-        'secondContent' => getPost()->field('downloads_content_2', 'content'),
-        'secondContentImage' => getPost()->field('downloads_content_image_2', 'image_array'),
-         'buttonsAsRows' => getPost()->field('downloads_buttons_as_rows', 'bool'),
-        'downloadButton' => (object) [
-            'label' => getPost()->field('downloads_download_button_label', 'text'),
-            'formId' => getPost()->field('right_hubspot'),
-            'deactivateUntil' => $deactivateDownloadingUntil
-        ],
-        'firstCheckoutButton' => (object) [
-            'label' => getPost()->field('downloads_buy_button_label', 'text'),
-            'product' => $productVariation,
-            'onClick' => "addMagazinProductToCart(" . (int) $productVariation->get_id() . ", 'variation')"
-        ],
-        'secondCheckoutButton' => (object) [
-            'label' => getPost()->field('downloads_buy_button_2_label', 'text'),
-            'product' => $magazineProduct,
-            'onClick' => "addMagazinProductToCart(" . (int) $magazineProduct->get_id() . ")"
-        ]
-    ]) ?>
+    <?php
+    if ($productVariation) {
+        echo DownloadView::loadTemplate('single', [
+            'title' => get_the_title(),
+            'leftImageUrl' => getPost()->field('left_image', 'image_array'),
+            'content' => getPost()->field('inhaltseditor', 'content'),
+            'peopleHeadline' => getPost()->field('downloads_people_headline', 'text'),
+            'people' => getPost()->field('downloads_people', 'repeater'),
+            'reviews' => getPost()->field('downloads_reviews', 'repeater'),
+            'firstContent' => getPost()->field('downloads_content_1', 'content'),
+            'firstContentImage' => getPost()->field('downloads_content_image_1', 'image_array'),
+            'secondContent' => getPost()->field('downloads_content_2', 'content'),
+            'secondContentImage' => getPost()->field('downloads_content_image_2', 'image_array'),
+            'buttonsAsRows' => getPost()->field('downloads_buttons_as_rows', 'bool'),
+            'downloadButton' => (object) [
+                'label' => getPost()->field('downloads_download_button_label', 'text'),
+                'formId' => getPost()->field('right_hubspot'),
+                'deactivateUntil' => $deactivateDownloadingUntil
+            ],
+            'firstCheckoutButton' => (object) [
+                'label' => getPost()->field('downloads_buy_button_label', 'text'),
+                'product' => $productVariation,
+                'onClick' => "addMagazinProductToCart(" . (int) $productVariation->get_id() . ", 'variation')"
+            ],
+            'secondCheckoutButton' => (object) [
+                'label' => getPost()->field('downloads_buy_button_2_label', 'text'),
+                'product' => $magazineProduct,
+                'onClick' => "addMagazinProductToCart(" . (int) $magazineProduct->get_id() . ")"
+            ]
+        ]); } else {
+        echo DownloadView::loadTemplate('single', [
+            'title' => get_the_title(),
+            'leftImageUrl' => getPost()->field('left_image', 'image_array'),
+            'content' => getPost()->field('inhaltseditor', 'content'),
+            'peopleHeadline' => getPost()->field('downloads_people_headline', 'text'),
+            'people' => getPost()->field('downloads_people', 'repeater'),
+            'reviews' => getPost()->field('downloads_reviews', 'repeater'),
+            'firstContent' => getPost()->field('downloads_content_1', 'content'),
+            'firstContentImage' => getPost()->field('downloads_content_image_1', 'image_array'),
+            'secondContent' => getPost()->field('downloads_content_2', 'content'),
+            'secondContentImage' => getPost()->field('downloads_content_image_2', 'image_array'),
+            'buttonsAsRows' => getPost()->field('downloads_buttons_as_rows', 'bool'),
+            'downloadButton' => (object) [
+                'label' => getPost()->field('downloads_download_button_label', 'text'),
+                'formId' => getPost()->field('right_hubspot'),
+                'deactivateUntil' => $deactivateDownloadingUntil
+            ],
+            'secondCheckoutButton' => (object) [
+                'label' => getPost()->field('downloads_buy_button_2_label', 'text'),
+                'product' => $magazineProduct,
+                'onClick' => "addMagazinProductToCart(" . (int) $magazineProduct->get_id() . ")"
+            ]
+        ]);
+    } ?>
 <?php else : ?>
     <div id="content" xmlns:background="http://www.w3.org/1999/xhtml">
         <div id="inner-content" class="wrap clearfix no-hero">
@@ -72,33 +98,33 @@ if ($magazineProduct || $productVariation) {
                                 <div class="navigation-tabs-section">
                                     <div class="navigation-tabs">
                                         <?php if (!$deactivateDownloadingUntil || $deactivateDownloadingUntil <= Date::get()) : ?>
-                                            <button 
-                                                class="navigation-tab button button-dark-blue" 
-                                                data-active="button-dark-blue" 
-                                                data-inactive="button-grey" 
-                                                onclick="navigateToTab('online-tab'); hideMagazinCheckout()"
+                                            <button
+                                                    class="navigation-tab button button-dark-blue"
+                                                    data-active="button-dark-blue"
+                                                    data-inactive="button-grey"
+                                                    onclick="navigateToTab('online-tab'); hideMagazinCheckout()"
                                             >
                                                 Online
                                             </button>
                                         <?php endif ?>
 
                                         <?php if ($productVariation) : ?>
-                                            <button 
-                                                class="navigation-tab button button-red" 
-                                                data-active="button-dark-blue" 
-                                                data-inactive="button-red" 
-                                                onclick="navigateToTab('print-edition-tab'); addMagazinProductToCart(<?php echo (int) $productVariation->get_id() ?>, 'variation')"
+                                            <button
+                                                    class="navigation-tab button button-red"
+                                                    data-active="button-dark-blue"
+                                                    data-inactive="button-red"
+                                                    onclick="navigateToTab('print-edition-tab'); addMagazinProductToCart(<?php echo (int) $productVariation->get_id() ?>, 'variation')"
                                             >
                                                 Printausgabe
                                             </button>
                                         <?php endif ?>
 
                                         <?php if ($magazineProduct) : ?>
-                                            <button 
-                                                class="navigation-tab button button-grey" 
-                                                data-active="button-dark-blue" 
-                                                data-inactive="button-grey" 
-                                                onclick="navigateToTab('subscribe-tab'); addMagazinProductToCart(<?php echo (int) $magazineProduct->get_id() ?>)"
+                                            <button
+                                                    class="navigation-tab button button-grey"
+                                                    data-active="button-dark-blue"
+                                                    data-inactive="button-grey"
+                                                    onclick="navigateToTab('subscribe-tab'); addMagazinProductToCart(<?php echo (int) $magazineProduct->get_id() ?>)"
                                             >
                                                 Abonnieren
                                             </button>
@@ -108,7 +134,7 @@ if ($magazineProduct || $productVariation) {
                                         <?php if (!$deactivateDownloadingUntil || $deactivateDownloadingUntil <= Date::get()) : ?>
                                             <div id="online-tab" class="navigation-tab-content">
                                                 <!--[if lte IE 8]>
-                                                    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
+                                                <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script>
                                                 <![endif]-->
                                                 <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
                                                 <script>
@@ -134,10 +160,10 @@ if ($magazineProduct || $productVariation) {
                                     </div>
                                 </div>
 
-                                <div 
-                                    id="omt-magazin-checkout" 
-                                    class="woocommerce-embed-checkout"
-                                    style="display: <?php echo (!$deactivateDownloadingUntil || $deactivateDownloadingUntil <= Date::get()) ? 'none' : 'block' ?>" 
+                                <div
+                                        id="omt-magazin-checkout"
+                                        class="woocommerce-embed-checkout"
+                                        style="display: <?php echo (!$deactivateDownloadingUntil || $deactivateDownloadingUntil <= Date::get()) ? 'none' : 'block' ?>"
                                 >
                                     <?php echo do_shortcode('[woocommerce_checkout]') ?>
                                 </div>
