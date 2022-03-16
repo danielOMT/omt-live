d52285bdf9480ab66b390fafc85fe3ac0223f850
danielOMT <daniel.voelskow@reachx.de>
2022-03-16 15:11:23 +0100

    add link to author on zitat shortcode + add corresponding field on editor addon

--- library/shortcodes/shortcode-zitat.php
+++ library/shortcodes/shortcode-zitat.php
@@ -17,7 +17,7 @@ function omt_zitat_shortcode( $atts ) {
                         </div>
         <div>
             <p class="citation style-<?php print $atts['farbe'];?>">&#8222;<?php print $atts['text'];?>&#8221;</p>
-        <p class="citation-author"><?php if (strlen($atts['link'])>0) { ?><a href="<?php print $atts['link'];?>"><?php } ?><?php print $atts['author'];?><?php if (strlen($atts['link'])>0) { print "</a>"; }?></p>
+        <p class="citation-author"><?php if (strlen($atts['link'])>0) { ?><a target="_blank" href="<?php print $atts['link'];?>"><?php } ?><?php print $atts['author'];?><?php if (strlen($atts['link'])>0) { print "</a>"; }?></p>
         </div>
     </div>
     <?php $result = ob_get_clean();
