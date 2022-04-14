22ade06e020702326052db45e23a6bd45c0c805b
gogaloo27 <goga.iosava@omt.de>
2022-04-15 03:53:58 +0400

    Update module-vertical-tabs.php

--- library/modules/module-vertical-tabs.php
+++ library/modules/module-vertical-tabs.php
@@ -16,7 +16,7 @@
 </div>
 <?php foreach ($tabs as $tab) :?>
 	<div id="tab_<?=$count_content;?>" class="tabcontent-omt">
-		<img src="<?=$tab['tab_image']['url']?>" alt="<?$tab['tab_lin'];?>" width="550" height="290">
+		<img src="<?=$tab['tab_image']['url']?>" alt="<?=$tab['tab_lin'];?>" width="550" height="290">
 	    <p><?=$tab['tab_content'];?></p>
   	</div>
 <?php $count_content++;
