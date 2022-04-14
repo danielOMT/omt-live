149a05d97e21c322957739a44fd4e4d182626b52
gogaloo27 <goga.iosava@omt.de>
2022-04-15 03:13:05 +0400

    Update scripts.js

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -1974,15 +1974,6 @@ window.onload = function() {
 $(document).on("gform_confirmation_loaded", function (e, form_id) {
   jQuery( ".checkout" ).addClass( "show_checkout" );
   jQuery( ".woocommerce-form-coupon-toggle" ).addClass( "show_checkout" );
-});window.onload = function() {
-    $rand_id = Math.floor(100000 + Math.random() * 900000);
-    document.getElementById("input_24_31").value = $rand_id;
-    document.getElementById("billing_uniqe_id_for_job").value = $rand_id;
-} 
-//After gform success display checkout
-$(document).on("gform_confirmation_loaded", function (e, form_id) {
-  jQuery( ".checkout" ).addClass( "show_checkout" );
-  jQuery( ".woocommerce-form-coupon-toggle" ).addClass( "show_checkout" );
 });
 
 
