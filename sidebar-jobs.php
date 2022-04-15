521a4e693a14caba8afc0fd57f90aa6a10407354
gogaloo27 <goga.iosava@omt.de>
2022-04-15 05:04:41 +0400

    Update sidebar-jobs.php

--- sidebar-jobs.php
+++ sidebar-jobs.php
@@ -25,21 +25,23 @@
 				   		<div >
 						   	<input type="checkbox"  name="category" value="<?=$cat->name; ?>" id="<?=$cat->term_id;?>" class="omt-input jobs_filter cat_f <?php if($count_cat > 2){ echo $hide_cat; }?>" />
 						   	<label class="cat_f <?php if($count_cat > 2){ echo $hide_cat; }?>" for="<?=$cat->term_id;?>"><?=$cat->name; ?> 
-						   		<label id="showCat_<?=$cat->term_id;?>" class="post_count category_c <?= clean($cat->name)?>">(<?=countByCategory($cat->name);?>)</label>
+						   		<label id="showCat_<?=$cat->term_id;?>" class="post_count category_c <?=clean($cat->name)?>">(<?=countByCategory($cat->name);?>)</label>
 						    </label>
 					   </div>
 						<?php  $count_cat++;
 					endforeach;?>
-					<button class="show_categories" onclick="show_cat()"> <span>Mehr anzeigen</span> <i class="arrow_ down_"></i></button>
-					<button style="display: none;" class="hide_categories" onclick="hide_cat()"> <span>Weniger anzeigen</span> <i class="arrow_ up_"></i></button>
 
-
+					<?php if($count_cat > 2):?>
+						<button class="show_categories" onclick="show_cat()"> <span>Mehr anzeigen</span> <i class="arrow_ down_"></i></button>
+						<button style="display: none;" class="hide_categories" onclick="hide_cat()"> <span>Weniger anzeigen</span> <i class="arrow_ up_"></i></button>
+					<?php endif;?>
+					
 					<div class="space_"></div>
 					<h5 class="filter_title"><?php _e('Erfahrung', 'jobs'); ?></h5>
 					<?php foreach ($choices as $choice) : ?>
 						<input type="checkbox"  name="erfahrung" value="<?=$choice['value'];?>" class="omt-input jobs_filter" id="erf_<?=$count_erf;?>"/>
 				   		<label class="cat_f" for="erf_<?=$count_erf;?>"><?=$choice['text'];?>
-				   			<label id="showErf_<?=$count_erf;?>" class="post_count erfahrung_c <?=$choice['value'];?>">(<?=countJobByErfahrung($choice['value']);?>)</label>
+				   			<label id="showErf_<?=$count_erf;?>" class="post_count erfahrung_c <?=clean($choice['value']);?>">(<?=countJobByErfahrung($choice['value']);?>)</label>
 				   		</label><br>
 					<?php $count_erf++; endforeach;?>
 
@@ -55,7 +57,7 @@
 					<?php foreach ($occupations as $occupation) : ?>
 						<input type="checkbox" name="occupation" value="<?=$occupation['value'];?>" class="omt-input jobs_filter" id="ccu_<?=$count_der;?>"/>
 				   		<label class="cat_f" for="ccu_<?=$count_der;?>"><?=$occupation['text'];?>
-				   			<label id="showBesch_<?=$count_der;?>" class="post_count besch_c <?=$occupation['value'];?>">(<?=countJobByBeschäftigung($occupation['value']);?>)</label>
+				   			<label id="showBesch_<?=$count_der;?>" class="post_count besch_c <?=clean($occupation['value']);?>">(<?=countJobByBeschäftigung($occupation['value']);?>)</label>
 				   		</label><br>
 					<?php $count_der++; endforeach;?>
 
