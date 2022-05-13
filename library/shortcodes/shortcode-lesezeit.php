3eb93cd20c5a01f797b79259f933db3f7150d3b3
danielOMT <daniel.voelskow@reachx.de>
2022-05-13 14:51:23 +0200

    possibility to deactivate lesezeit + add shortcode for it

--- library/shortcodes/shortcode-lesezeit.php
+++ library/shortcodes/shortcode-lesezeit.php
@@ -7,7 +7,10 @@ function shortcode_lesezeit( $atts ) {
     );
 
     ob_start(); ?>
-    <p style="color: #ea506c !important;"><strong>Lesezeit: <?php echo reading_time(get_the_ID());?></strong></p>
+    <div class="info-wrap">
+    <strong>Lesezeit: <?php echo reading_time(get_the_ID());?></strong>
+    <div class="socials-header"><?php print do_shortcode('[shariff headline="Teile den Artikel" borderradius="1" buttonsize="small" services="facebook|twitter|googleplus|linkedin|xing" theme="round" orientation="horizontal" align="flex-end"]');?></div>
+    </div>
     <?php $result = ob_get_clean();
 
 
