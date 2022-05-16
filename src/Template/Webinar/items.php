39533fd0f93432f88353f5cfb56776e68f6863e9
danielOMT <daniel.voelskow@reachx.de>
2022-05-16 13:28:31 +0200

    prevent webinars from being shown twice

--- src/Template/Webinar/items.php
+++ src/Template/Webinar/items.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 use OMT\View\WebinarView;
 ?>
@@ -17,35 +17,40 @@ wp_enqueue_script('alpine-lines-clamping', get_template_directory_uri() . '/libr
 wp_enqueue_script('alpinejs', get_template_directory_uri() . '/library/js/libs/alpine.min.js');
 ?>
 <?php if (count($this->webinars)) : ?>
+    <?php $shownwebinars = array(); ?>
     <?php foreach ($this->webinars as $key => $webinar) : ?>
-        <?php if ($this->highlightFirst && $key === 0) : ?>
-            <?php echo WebinarView::loadTemplate('highlighted-item', [
-                'webinar' => $webinar,
-                'position' => ($this->offset ?? 0) + $key + 1
-            ]) ?>
-        <?php else : ?>
-            <?php echo WebinarView::loadTemplate('item', [
-                'webinar' => $webinar,
-                'highlightFirst' => $this->highlightFirst,
-                'isFirst' => $key === 0,
-                'position' => ($this->offset ?? 0) + $key + 1
-            ]) ?>
-        <?php endif ?>
+        <?php if (!in_array($this->webinar->extra->url, $shownwebinars)) {
+            array_push($shownwebinars, $this->webinar->extra->url);
+            ?>
+            <?php if ($this->highlightFirst && $key === 0) : ?>
+                <?php echo WebinarView::loadTemplate('highlighted-item', [
+                    'webinar' => $webinar,
+                    'position' => ($this->offset ?? 0) + $key + 1
+                ]) ?>
+            <?php else : ?>
+                <?php echo WebinarView::loadTemplate('item', [
+                    'webinar' => $webinar,
+                    'highlightFirst' => $this->highlightFirst,
+                    'isFirst' => $key === 0,
+                    'position' => ($this->offset ?? 0) + $key + 1
+                ]) ?>
+            <?php endif ?>
+        <?php } ?>
     <?php endforeach ?>
 
     <?php if (isset($this->loadMoreWebinars) && $this->loadMoreWebinars) : ?>
         <div x-data="xLoadWebinars()" style="display:block;width:100%;">
-            <button 
-                type="button"
-                @click="load"
-                class="button button-gradient button-730px button-center button-loadmore webinare-loadmore"
-                data-offset="<?php echo $this->limit ?>"
-                data-categories="<?php echo $this->categories ? implode(',', $this->categories) : '' ?>"
+            <button
+                    type="button"
+                    @click="load"
+                    class="button button-gradient button-730px button-center button-loadmore webinare-loadmore"
+                    data-offset="<?php echo $this->limit ?>"
+                    data-categories="<?php echo $this->categories ? implode(',', $this->categories) : '' ?>"
             >
                 Mehr laden
             </button>
         </div>
-        
+
         <div class="teaser-loadmore webinare-results x-w-full">
             <div class="webinare-ajax-status"><i class="fa fa-circle-o-notch fa-spin fa-fw" style="vertical-align:middle;"></i>Webinare werden geladen</div>
         </div>
