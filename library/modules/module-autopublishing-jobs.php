e65c64a5c0c94a43424452fd430368a5b1f295e4
danielOMT <daniel.voelskow@reachx.de>
2022-05-25 14:48:03 +0200

    modify autojobs pricing table module

--- library/modules/module-autopublishing-jobs.php
+++ library/modules/module-autopublishing-jobs.php
@@ -9,9 +9,7 @@ $active = true;
 $countdown_im_header = get_field('countdown_im_header');
 $countdown_download_button_url = $countdown_im_header ? getPost()->field('countdown_download_button_url') : '';
 $countdown_download_button_label = $countdown_im_header ? getPost()->field('countdown_download_button_label') : '';
-?>
-    <span class="anchor" id="ticket"></span>
-<?php
+
 foreach ($variations1 as $ticketvariation) :   /*build array with all seminars and all repeater date fields*/
     //collecting data
     $single_variation = new WC_Product_Variation($ticketvariation);
