9f8ae92fe7b6ec3341f19b7b874acb850533d7a2
danielOMT <daniel.voelskow@reachx.de>
2022-06-03 16:46:14 +0200

    add module "auswahlboxen"

--- library/modules/module-auswahlboxen.php
+++ library/modules/module-auswahlboxen.php
@@ -7,9 +7,9 @@
 ////link
 ///
 foreach ($zeile['inhaltstyp'][0]['auswahlboxen'] as $item) { ?>
-    <div class="auswahlbox card">
+    <a class="auswahlbox card" href="<?php print $item['link'];?>">
         <img src="<?php print $item['bild']['url'];?>" alt="<?php print $item['bild']['alt'];?>" title="<?php print $item['bild']['alt'];?>" />
         <strong style="display:block;text-align:center;"><?php print $item['titel'];?></strong>
         <?php print $item['text'];?>
-    </div>
+    </a>
 <?php } ?>
