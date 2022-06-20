803a678d93662c789355cca3c9b631bf58b71989
danielOMT <daniel.voelskow@reachx.de>
2022-06-20 17:04:10 +0200

    change content select module

--- library/modules/module-auswahlboxen.php
+++ library/modules/module-auswahlboxen.php
@@ -9,7 +9,7 @@
 foreach ($zeile['inhaltstyp'][0]['auswahlboxen'] as $item) { ?>
     <a class="auswahlbox card" href="<?php print $item['link'];?>">
         <img src="<?php print $item['bild']['url'];?>" alt="<?php print $item['bild']['alt'];?>" title="<?php print $item['bild']['alt'];?>" />
-        <strong style="display:block;text-align:left;"><?php print $item['titel'];?></strong>
+        <strong style="display:block;text-align:left;margin-bottom:10px;"><?php print $item['titel'];?></strong>
         <?php print $item['text'];?>
     </a>
 <?php } ?>
