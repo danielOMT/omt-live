d5d5dc24db9a7ab0b84f3c28326775f32c4b2e53
danielOMT <daniel.voelskow@reachx.de>
2022-06-27 16:44:42 +0200

    use product activation checkboxes in variations

--- library/modules/module-konferenzticket.php
+++ library/modules/module-konferenzticket.php
@@ -45,25 +45,25 @@ foreach ($variations1 as $ticketvariation) {   /*build array with all seminars a
     if ( $lager <1 OR $active != true) { $inactive_class = "ticket-inactive"; }
     ?>
     <div class="teaser teaser-<?php print $teaser_size;?> ticket <?php if ($lager<1 OR $active != true) { print "ticket-inactive"; } ?>" <?php print $style; ?>>
-    <?php if ($lager > 0 && $active == true) { ?>
-        <a data-ticket-type="<?php print $ticketkategorie;?>" href="/kasse/?add-to-cart=<?php print $ticket_variation_id;?>&attribute_pa_leistungszeitraum=<?php print $leistungszeitraum;?>&attribute_pa_ticketkategorie=<?php print $ticketkategorie;?>&attribute_pa_typ=<?php print $typ;?>" title="<?php the_title_attribute(); ?>">
-    <?php } ?>
-    <?php /*<img class="teaser-img" title="<?php print $ticketkategorie;?>" alt="<?php print $ticketkategorie;?>" src="<?php print wp_get_attachment_image_src($ticket_img_id, 'full')[0];?>"/>
-        <span class="teaser-cat"><?php print $product->post_title;?></span>*/?>
-        <h4 class="ticket-type"><?php print get_the_category_by_ID($ticketkategorie);?></h4>
-        <p class="ticket-price"><?php print $preis;?>,- &euro;</p>
-        <p class="ticket-info"><span class="ticket-description"><?php if (strlen($beschreibung)>0) { print strip_tags($beschreibung) . "</span><br/>"; }?><?php
-            if ( ( 215211 == $ticket_variation_id) OR ( 201349 == $ticket_variation_id) OR ( 215209 == $ticket_variation_id) ) {
-                print "*unbegrenzt verfügbar";
-
-            } else {
-                print $lager . "/" . $stock_max . " verfügbar"; } ?></p>
         <?php if ($lager > 0 && $active == true) { ?>
-            <span data-ticket-type="<?php print $ticketkategorie;?>" class="button button-gradient" title="<?php the_title_attribute(); ?>">Ticket Kaufen</span>
-        <?php } else { ?><div class="button button-gradient"><?php if ($lager > 0) { print "nicht verfügbar"; } else { print "ausverkauft!";}?></div><?php } ?>
-        <?php if (!empty($countdown_download_button_url) && !empty($countdown_download_button_label)) : ?>
-            </a><a class="button button-red x-mr-4" href="<?php echo $countdown_download_button_url ?>" target="_blank"><?php echo $countdown_download_button_label ?></a>
-        <?php endif ?>
+        <a data-ticket-type="<?php print $ticketkategorie;?>" href="/kasse/?add-to-cart=<?php print $ticket_variation_id;?>&attribute_pa_leistungszeitraum=<?php print $leistungszeitraum;?>&attribute_pa_ticketkategorie=<?php print $ticketkategorie;?>&attribute_pa_typ=<?php print $typ;?>" title="<?php the_title_attribute(); ?>">
+            <?php } ?>
+            <?php /*<img class="teaser-img" title="<?php print $ticketkategorie;?>" alt="<?php print $ticketkategorie;?>" src="<?php print wp_get_attachment_image_src($ticket_img_id, 'full')[0];?>"/>
+        <span class="teaser-cat"><?php print $product->post_title;?></span>*/?>
+            <h4 class="ticket-type"><?php print get_the_category_by_ID($ticketkategorie);?></h4>
+            <p class="ticket-price"><?php print $preis;?>,- &euro;</p>
+            <p class="ticket-info"><span class="ticket-description"><?php if (strlen($beschreibung)>0) { print strip_tags($beschreibung) . "</span><br/>"; }?><?php
+                if ( ( 215211 == $ticket_variation_id) OR ( 201349 == $ticket_variation_id) OR ( 215209 == $ticket_variation_id) ) {
+                    print "*unbegrenzt verfügbar";
+
+                } else {
+                    print $lager . "/" . $stock_max . " verfügbar"; } ?></p>
+            <?php if ($lager > 0 && $active == true) { ?>
+                <span data-ticket-type="<?php print $ticketkategorie;?>" class="button button-gradient" title="<?php the_title_attribute(); ?>">Ticket Kaufen</span>
+            <?php } else { ?><div class="button button-gradient"><?php if ($lager > 0) { print "nicht verfügbar"; } else { print "ausverkauft!";}?></div><?php } ?>
+            <?php if (!empty($countdown_download_button_url) && !empty($countdown_download_button_label)) : ?>
+        </a><a class="button button-red x-mr-4" href="<?php echo $countdown_download_button_url ?>" target="_blank"><?php echo $countdown_download_button_label ?></a>
+    <?php endif ?>
         <?php if ($beschreibungselemente != 0) {
             ?><div class="produkt-beschreibung"><?php
             foreach ($beschreibungselemente as $element) {
