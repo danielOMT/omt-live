d425c4d5b3a7c1a1ad942905910b0963db48d66d
danielOMT <daniel.voelskow@reachx.de>
2022-06-30 15:17:37 +0200

    adding sidebar and buttons to tool alternativen pages

--- single-tool-alternativen.php
+++ single-tool-alternativen.php
@@ -139,7 +139,7 @@ if (empty($mobile_banner_bild) && (get_field('globales_banner_verwenden_mobile')
                 flex-wrap: wrap;
                 align-content: flex-start;
             ">
-                    <?php if (strlen($tool_gratis_testen_link) > 0) { ?><a style="width:225px;" rel="nofollow" id="<?php print get_the_title();?>"  target="_blank" class="button button-testen button-red button-350px"  href="<?php print $tool_gratis_testen_link;?>">Gratis testen</a><?php } ?>
+                    <?php if (strlen($tool_gratis_testen_link) > 0) { ?><a style="width:225px;margin-right:0px !important;" rel="nofollow" id="<?php print get_the_title();?>"  target="_blank" class="button button-testen button-red button-350px"  href="<?php print $tool_gratis_testen_link;?>">Gratis testen</a><?php } ?>
                     <?php if (strlen($tool_preisubersicht) > 0) { ?><a style="width:225px;margin-top:10px;" rel="nofollow" id="<?php print get_the_title();?>" target="_blank" class="button button-pricing button-lightgrey button-350px"  href="<?php print $tool_preisubersicht;?>">Preisübersicht</a><?php } ?>
                 </div>
             <?php }
