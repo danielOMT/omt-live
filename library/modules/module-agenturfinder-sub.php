9f0b8d709018a6065784daaf29b8e95571409d1f
gogaloo27 <99730967+gogaloo27@users.noreply.github.com>
2022-07-01 11:04:38 +0400

    Update module-agenturfinder-sub.php

--- library/modules/module-agenturfinder-sub.php
+++ library/modules/module-agenturfinder-sub.php
@@ -100,12 +100,12 @@ foreach ($variations1 as $ticketvariation) :   /*build array with all seminars a
                                 
                                 <?php  if($preis != 0 && $m_price > 0 && $product_class != 'monthly'):?>
 
-                                <span class="an_billed"><?=$m_price*12;?>&euro; 
+                                 
                                 <!-- <span class="s_billed quarterly_"><?//= __('Jährlich');?></span>
                                 <span class="s_billed halfyearly_"><?//= __('Jährlich');?></span>
                                 <span class="s_billed yearly_"><?//= __('Jährlich');?></span></span> -->
                             <?php else:?>
-
+                                <span class="an_billed"><?=$m_price*12;?>&euro;
                             <?php endif;?>
                             </p>
                         </div>
