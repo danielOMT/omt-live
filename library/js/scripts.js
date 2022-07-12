682c9c315e2c78368926f433e743761eccbf7c7c
gogaloo27 <goga.iosava@omt.de>
2022-07-12 17:52:05 +0400

    Update scripts.js

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -2155,13 +2155,6 @@ $( document ).ready(function() {
 
 
 
-$( document ).ready(function() {
-    alert();
-    $(".checkbox-rec_vid").append("<span onclick='call_rec_video_modal()' class='rec_video_modal_icon'> &#8505;</span>");
-});
-function call_rec_video_modal(){
-  $(".rec_video_link").trigger("click");  
-}
 
 
 
