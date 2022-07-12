7cd196ba18717b23917b9200a3de0f08131e1558
gogaloo27 <goga.iosava@omt.de>
2022-07-12 17:19:59 +0400

    Update theme-woocommerce.php

--- library/theme-woocommerce.php
+++ library/theme-woocommerce.php
@@ -1005,7 +1005,7 @@ function getDescriptionForRecVideo($description){
 function test(){
     echo "<script>$( document ).ready(function() {
     alert();
-    $('.checkbox-rec_vid').append('<span onclick="'call_rec_video_modal()'" class="'rec_video_modal_icon'"> &#8505;</span>');
+    $('.checkbox-rec_vid').append('<span onclick='call_rec_video_modal()' class='rec_video_modal_icon'> &#8505;</span>');
 });
 function call_rec_video_modal(){
   $('.rec_video_link').trigger('click');  
