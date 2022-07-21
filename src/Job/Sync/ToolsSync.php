28f11f01148b70c3733f13d3601a63ab2335b488
danielOMT <daniel.voelskow@reachx.de>
2022-07-21 18:41:10 +0200

    run a function on acf_form saving?

--- src/Job/Sync/ToolsSync.php
+++ src/Job/Sync/ToolsSync.php
@@ -16,7 +16,7 @@ class ToolsSync extends Job
         // Initialize hook later (priority = 15) to have all the actual data from other plugins like ACF
         add_action('save_post', [$this, 'save'], 15, 3);
 
-        add_action('acf/save_post' , 'save', 10, 1 );
+        add_action('acf/save_post' , [$this, 'save_acf'], 10, 1 );
 
         // Sync deletion (destroy) when the corresponding post has been deleted
         add_action('deleted_post', [$this, 'destroy'], 15, 2);
@@ -25,47 +25,27 @@ class ToolsSync extends Job
         add_action('wp_ajax_sync_all_tools', [$this, 'saveAll']);
     }
 
-    function save_acf($postId) {
+    function save_acf($postId, WP_Post $post) {
 //do action if acf_form is saved to update some fields so we can save category changes into the database!
-        add_action('acf/save_post' , 'my_pre_save_post', 10, 1 );
-        function my_pre_save_post( $post_id ) {
-            // bail early if not a contact_form post
-            // bail early if editing in admin
-            if( is_admin() ) {
-                return;
-            }
-            if (defined('DOING_AJAX') && DOING_AJAX) {
-                return;
-            }
-
-            if ($post->post_type !== Tool::init()->getPostTypeName()) {
-                return;
-            }
-
-            if (wp_is_post_revision($post)) {
-                return;
-            }
-
-            if (isset($post->post_status) && $post->post_status == 'auto-draft') {
-                return;
-            }
-
-            // Tool 191511 has been excluded from sync, logic is taken from JSON solution
-            if ($post->ID == 191511) {
-                return;
-            }
-
-            Tool::init()->sync($post);
-            // get custom fields (field group exists for content_form)
-            $name = "OMT saving post!";
-            $email = "info@omt.de";
-            $to = 'daniel.voelskow@reachx.de';
-            $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
-            $subject = "post was saved!";
-            $body = "some body content" . $post_id;
-            // send email
-            wp_mail($to, $subject, $body, $headers );
+        // bail early if not a contact_form post
+        // bail early if editing in admin
+        if( is_admin() ) {
+            return;
         }
+        if (defined('DOING_AJAX') && DOING_AJAX) {
+            return;
+        }
+
+        Tool::init()->sync($post);
+        // get custom fields (field group exists for content_form)
+        $name = "OMT saving post!";
+        $email = "info@omt.de";
+        $to = 'daniel.voelskow@reachx.de';
+        $headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n";
+        $subject = "post was saved!";
+        $body = "some body content" . $postId;
+        // send email
+        wp_mail($to, $subject, $body, $headers );
     }
 
     public function save($postId, WP_Post $post, bool $update)
