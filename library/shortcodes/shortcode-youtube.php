7c74cc3b1fcf60a852e5fef6e103d553384a692a
gogaloo27 <goga.iosava@omt.de>
2022-07-25 20:53:45 +0400

    Update shortcode-youtube.php

--- library/shortcodes/shortcode-youtube.php
+++ library/shortcodes/shortcode-youtube.php
@@ -2,20 +2,21 @@
 
 // Add Shortcode
 function omt_youtube( $atts ) {
+
     // Attributes
     $atts = shortcode_atts(
         array(
-            "id" => ""
+            "id" => "",
+            "class" => ""
         ),
         $atts
     );
-
-
+    $helper = strip_tags($atts['class']);
    /* return '<div class="vidembed_wrap ">
             <div class="vidembed">
                 <iframe title="YouTube video player" src="https://www.youtube.com/embed/'.$atts['id'].'?enablejsapi=1&origin=' . get_the_permalink() . '" frameborder="0"  allowfullscreen></iframe>
             </div></div>';*/
-   return '<div class="vidembed_wrap "><div class="youtube lazy-youtube helper-youtube" data-embed="' . $atts['id'] . '">
+   return '<div class="vidembed_wrap "><div class="youtube lazy-youtube '.$helper.'" data-embed="' . $atts['id'] . '">
 <div class="play-button"></div>
 </div></div>';
 
